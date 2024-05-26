73ec12703f157191bee8442eb9259ffee12fac86
Nefertiti Aramburo Leguizamo <neferititia02@gmail.com>
2024-05-25 23:46:57 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+/* escribir mis funciones
+funcion 1 : generar un color aleatorio */
+function generaColor(){
+// generar mi valor de RGB por separado y guardarlos en variables
+
+const rojo=;
+const verde= ;
+const azul= ;
+}
+// ---> RBG(X,Y,Z)
+/* funcion 2 : cambiar el color de findi del cuerpo de la página*/
+function cambioBackground(){
+// generaColor();
+
+}
+// buscar el botón en mi HTML por su clase
+// cada vez que mi botón sea accionado, voy a llamar o ejecutar mi funcion cambiBackground
